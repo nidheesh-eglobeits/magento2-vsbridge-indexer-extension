def835cd8981a4e1b591b0eea41afde74be13b15
Jan Myszkier <janmyszkier@gmail.com>
2020-01-05 15:35:18 +0100

    add registration.php for magento autoloader

---
+++ registration.php
@@ -0,0 +1,9 @@
+<?php
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::MODULE,
+    'CodingMice_VsBridgeIndexerExtension',
+    __DIR__
+);
